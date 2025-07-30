7a7e684a06fe8de5036e87bcc4450e560fd91224
jrx100th <jenith.ch15@gmail.com>
2025-07-30 14:31:56 +0530

    Section_23
    Node.js intro
    a proper code intro
    npm and more
    here i just removed the node modules from my 2.4 project folder.

---
+++ Section_23/2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,11 @@
+// var generateName = require('sillyname');
+
+// import generateName  from "sillname"
+// var sillyName = generateName();
+
+// console.log("My sillyname is " + sillyName)
+
+import superheroes from "superheroes"
+
+var name = superheroes[Math.floor(Math.random()*1800)];
+console.log("My Super hero name is "+name+"!")
---
+++ Section_23/2.4+QR+Code+Project/2.4 QR Code Project/My_project/index.js
@@ -0,0 +1,27 @@
+/* 
+1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/
+
+import {input} from '@inquirer/prompts';
+import {
+    writeFile,
+    createWriteStream,
+}  from "fs";
+import {
+    image,
+    imageSync
+} from "qr-image";
+
+const answer = await input({
+    message : "Enter the web address : "
+});
+
+writeFile("Site.txt",answer,(err)=>{
+    if (err) throw err;
+    console.log("file is saved");
+});
+
+var qr_png = image(answer,{type : 'png'});
+qr_png.pipe(createWriteStream("QR.png"));
